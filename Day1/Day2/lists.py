03da6a88c69c63e24fca233332859297bed34891
nnisuu123 <nisarga838@gmail.com>
2026-08-05 11:51:44 +0530

    list commit

---
+++ Day1/Day2/lists.py
@@ -0,0 +1,9 @@
+stda=[["name","akash"],["age",23],[["marks"],[23, 44,55]]]
+print("name:", stda[0][1])
+print("age:", stda[1][1])
+print("marks:")
+print(stda[2][1][0])
+print(stda[2][1][1])
+print(stda[2][1][2])
+
+
